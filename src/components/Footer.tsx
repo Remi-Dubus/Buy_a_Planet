39ddb973dc2d0bebbfd217943086b466fe59dfeb
REMI DUBUS <remdubus@gmail.com>
2025-01-16 13:40:08 +0100

    fix: bug on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,10 @@ export default function Footer() {
 		<>
 			<div className="bg-slate-900  text-yellow-100 p-8 flex flex-col items-center border-t-2 border-yellow-100">
 				<span>&copy; Rémi Dubus. </span>
-				<span className="flex flex-wrap justify-center">
-					Image conçu par{" "}
-					<a className="text-orange-400" href="https://pixabay.com/fr/">
-						Pixabay
-					</a>
-				</span>
+				<span className="flex flex-wrap justify-center">Image conçu par</span>
+				<a className="text-orange-400" href="https://pixabay.com/fr/">
+					Pixabay
+				</a>
 			</div>
 		</>
 	);
